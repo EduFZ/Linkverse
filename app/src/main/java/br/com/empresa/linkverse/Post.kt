d8143fd7d3df2f01dad6ffae9cb31d649df1f1fd
Vitor Henrique <vhenriquebm@gmail.com>
2022-09-04 12:06:24 -0300

    Retrofit e GSON importados, classe para receber posts criada

---
+++ app/src/main/java/br/com/empresa/linkverse/Post.kt
@@ -0,0 +1,17 @@
+package br.com.empresa.linkverse
+
+import com.google.gson.annotations.SerializedName
+
+data class Post (
+
+    @SerializedName("userId")   val userId: String,
+    @SerializedName("userId")   val id: String,
+    @SerializedName("userId")   val title: String,
+    @SerializedName("userId")   val body: String,
+
+        )
+
+
+
+
+
